57a3a218998cd55a43ceff9b67066a46e3666702
井上大誠 <t.base.8901@icloud.com>
2021-03-23 18:43:38 +0900

    remove gorm.Model

--- src/domain/model/user.go
+++ src/domain/model/user.go
@@ -1,11 +1,6 @@
 package model
 
-import (
-	"github.com/jinzhu/gorm"
-)
-
 type User struct {
-	gorm.Model
 	Email 	 string `gorm:"unique; not null"`
 	Name 	 string `gorm:"unique; not null"`
 	Password string `gorm:"unique; not null"`

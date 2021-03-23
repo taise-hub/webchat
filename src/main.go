a7493ce19f32e26875450b66e91e637cba96f8a6
井上大誠 <t.base.8901@icloud.com>
2021-03-23 20:30:07 +0900

    fix migration

--- src/main.go
+++ src/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"github.com/taise-hub/webchat/src/domain/model"
 	"github.com/taise-hub/webchat/src/infrastructure"
 	"github.com/taise-hub/webchat/src/interface/controller"
 )
 
 func main() {
 	db, err := infrastructure.NewDB(infrastructure.NewConfig())
+	db.AutoMigrate(&model.User{})
 	if err != nil {
 		panic(err)
 	}

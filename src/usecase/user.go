3d0737f91bef73a2be5b1532928b48052e0a3a27
井上大誠 <t.base.8901@icloud.com>
2021-03-23 19:29:58 +0900

    remove NewUserUsecse

--- src/usecase/user.go
+++ src/usecase/user.go
@@ -6,21 +6,21 @@ import (
 	"github.com/taise-hub/webchat/src/domain/repository"
 )
 
-
-type userUsecase struct {
-	rep repository.UserRepository
+type UserUsecase struct {
+	Repository repository.UserRepository
 }
 
-func (us *userUsecase) GetByEmail(email string) (*model.User, error) {
-	user, err := us.rep.GetByEmail(email)
+
+func (us *UserUsecase) GetByEmail(email string) (*model.User, error) {
+	user, err := us.Repository.GetByEmail(email)
 	if err != nil {
 		return nil, err
 	}
 	return user, nil
 }
 
-func (us *userUsecase) Create(user *model.User) error {
-	 if err := us.rep.Create(user); err != nil {
+func (us *UserUsecase) Create(user *model.User) error {
+	 if err := us.Repository.Create(user); err != nil {
 		 return err
 	 }
 	 return nil
